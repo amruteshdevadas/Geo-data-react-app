23f9a1f3a408c36bf96b81eb52b7e23d0c7a43fb
Amrutesh Devadas <amrutesh.devadas@gmail.com>
2024-08-16 08:26:04 +0530

    fix: issues

--- geo-data-react-app/src/components/MapComponent/index.jsx
+++ geo-data-react-app/src/components/MapComponent/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMap } from "react-leaflet/hooks";
 import "leaflet/dist/leaflet.css";
+
+function MyComponent() {
+  const map = useMap();
+  console.log("map center:", map.getCenter());
+  return null;
+}
 
 export const MapComponent = () => {
   return (
@@ -9,7 +16,14 @@ export const MapComponent = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker
+        position={[51.505, -0.09]}
+        eventHandlers={{
+          click: (e) => {
+            console.log("marker Clicked", e);
+          },
+        }}
+      >
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
